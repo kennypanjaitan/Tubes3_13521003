33aca63202735a775de5690a03710f63382f9f72
kennypanjaitan_ <13521023@std.stei.itb.ac.id>
2023-04-25 04:38:59 +0700

    feat: add configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        'day': ['day1', 'sans-serif']
       },
-    },
+      colors: {
+        'primary-bg': '#F8F3E8',
+        'sidebar-bg': '#BD7637',
+        'text-color': '#FFFFFF',
+        'user-bg'   : '#9F9B92',
+        'bot-bg'    : '#4D4C47',
+      },
+      width: {
+        'sidebar': '571px'
+      }
+    }
   },
   plugins: [],
 }
