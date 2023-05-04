4d4c1c800e2e0eeb168467dea83ac29eaa304546
Eunice Sarah Siregar <13521013@mahasiswa.itb.ac.id>
2023-05-04 14:59:16 +0700

    feat: update regex on date

--- src/backend/date.js
+++ src/backend/date.js
@@ -26,7 +26,47 @@ function monthToInt(month) {
     const calculate = ((year + Math.floor(year / 4) - Math.floor(year / 100) + Math.floor(year / 400) + magicNumber[month - 1] + day) % 7);
     return days[calculate];
   }
+
+  function validateDate(date) {
+    const dateRegex = /\b(\d{1,2})\/(\d{1,2})\/(\d{4})\b/;
+    const dateMatch = date.match(dateRegex);
+    if (dateMatch === null) {
+      return false;
+    }
+    const day = parseInt(dateMatch[1]);
+    const month = parseInt(dateMatch[2]);
+    const year = parseInt(dateMatch[3]);
+    if (day < 1 || day > 31) {
+      return false;
+    }
+    if (month < 1 || month > 12) {
+      return false;
+    }
+    return true;
+  }
+
+  function main() {
+    const readline = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
   
-  const dayOfWeek = getDayOfWeek(201, 2, 29);
-  console.log(dayOfWeek); // Output: Friday
-  
+    readline.question('Please enter a string containing a date (DD/MM/YYYY): ', (inputString) => {
+      readline.close();
+      const dateRegex = /\b(\d{1,2})\/(\d{1,2})\/(\d{1,})\b/;
+      const dateMatch = inputString.match(dateRegex);
+     
+      const inputDate = dateMatch[0];
+      const dateParts = inputDate.split("/");
+      const day = parseInt(dateParts[0]);
+      const month = parseInt(dateParts[1]);
+      const year = parseInt(dateParts[2]);
+      if (!validateDate(inputDate)) {
+        console.log("Invalid input format. Please enter date in DD/MM/YYYY format.");
+        return;
+      }
+      const dayOfWeek = getDayOfWeek(year, month, day);
+      console.log(`The day of the week for ${inputDate} is ${dayOfWeek}.`);
+    });
+  }
+main();
